aec12cf6fe154e1733173dd410a7cfe6fcbbf1af
takahashi <takahashi@sproutgroup.co.jp>
2017-04-04 17:34:49 +0900

    Encode JSON value from struct

--- src/components/Daily.vue
+++ src/components/Daily.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="daily">
     <h1>{{date}}</h1>
-    <div v-for='person in members'>
-      <h2>{{person.name}}</h2>
+    <div v-for='entry in entries'>
+      <h2>{{entry.member.name}}</h2>
       <h3>昨日したこと</h3>
       <ul id='yesterday'>
-        <li v-for='work in person.yesterday'>{{work.title}}</li>
+        <li v-for='work in entry.yesterday'>{{work.title}}</li>
       </ul>
       <h3>今日すること</h3>
       <ul id='today'>
-      <li v-for='work in person.today'>{{work.title}}</li>
+      <li v-for='work in entry.today'>{{work.title}}</li>
       </ul>
       <h3>障害/業務外の予定</h3>
       <ul id='problem'>
-      <li v-for='issue in person.problem'>{{issue.title}}</li>
+      <li v-for='issue in entry.problem'>{{issue.title}}</li>
       </ul>
     </div>
   </div>
@@ -26,12 +26,12 @@ export default {
   data () {
     return {
       date: (new Date()).toLocaleDateString(),
-      members: []
+      entries: []
     }
   },
   mounted () {
     axios.get('data/').then(res => {
-      this.members = res.data
+      this.entries = res.data
     }).catch(err => {
       console.error(err)
     })
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
-#[macro_use] extern crate nickel;
-use nickel::{Nickel, HttpRouter, StaticFilesHandler, Mountable, MediaType, NickelError, Action, Halt, Request};
+#[macro_use]
+extern crate nickel;
+extern crate rustc_serialize;
+
+use nickel::{Nickel, HttpRouter, StaticFilesHandler, Mountable, MediaType, NickelError, Action, Halt, Request };
+use rustc_serialize::json;
+
+#[derive(RustcEncodable)]
+pub struct Member {
+    pub name: String,
+}
+
+#[derive(RustcEncodable)]
+pub struct Work {
+    pub title: String,
+}
+
+#[derive(RustcEncodable)]
+pub struct Issue {
+    pub title: String,
+}
+
+#[derive(RustcEncodable)]
+pub struct Entry {
+    pub member: Member,
+    pub yesterday: Vec<Work>,
+    pub today: Vec<Work>,
+    pub problem: Vec<Issue>
+}
 
 fn main() {
+    let daily = vec![Entry {
+        member: Member { name: "太郎".to_string() },
+        yesterday: vec![Work { title: "チケット#2".to_string() }, Work { title: "チケット#4".to_string() }],
+        today: vec![Work { title: "チケット#3".to_string() }],
+        problem: vec![Issue { title: "結合テスト環境が動いていない".to_string() }]
+    }];
     let mut srv = Nickel::new();
 
     // Serve contents build by "npm run build"
@@ -9,19 +42,9 @@ fn main() {
         println!("request: {:?}", request.origin.uri);
     });
     srv.mount("/", StaticFilesHandler::new("./dist"));
-    srv.get("/data/", middleware! {|request, mut response|
+    srv.get("/data/", middleware! {|_, mut response|
         response.set(MediaType::Json);
-        r#"[{
-    "name": "太郎",
-    "yesterday": [{
-        "title": "チケット#2"
-    },
-    {
-        "title": "チケット#4"
-    }],
-    "today": [{"title": "チケット#3"}],
-    "problem": [{"title": "結合テスト環境が動いていない"}]
-}]"#
+        json::encode(&daily).unwrap()
     });
 
     fn custom_404<'a>(err: &mut NickelError, _req: &mut Request) -> Action {
